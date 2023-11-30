1f9a284a9429fbd2b32d856be9604bdeccf0e23a
kmax3ever <kmax3ever2@gmail.com>
2023-12-01 00:01:52 +0700

    fix hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,48 +10,13 @@ export default {
     },
     deploy: {
       url: process.env.RPC_ENDPOINT,
-      gasPrice: 10000000000,
-      gas: 10000000,
+      // gasPrice: 10000000000,
+      // gas: 10000000,
       chainId: Number(process.env.CHAIN_ID),
       accounts: [process.env.PKEY as string],
       allowUnlimitedContractSize: true,
     },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    arbitrumRinkeby: {
-      url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    arbitrum: {
-      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    optimismKovan: {
-      url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    optimism: {
-      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    bnb: {
-      url: `https://bsc-dataseed.binance.org/`,
-    },
+    
   },
   etherscan: {
     // Your API key for Etherscan
